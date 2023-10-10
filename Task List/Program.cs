39e8d2358a6c0a9e4e2393ca7e5c5903a1ddc96d
capralhachi <romanbdiukhin@gmail.com>
2023-10-10 20:42:41 +0200

    Add project files.

---
+++ Task List/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_List
+{
+	class Program : Task
+	{
+		static void Main(string[] args)
+		{
+            List<string> toDoList = new List<string>();
+
+			Task task = new Task();
+
+			while (true)
+			{
+				Console.WriteLine("Choose an option!");
+				Console.WriteLine("1. Add task!");
+				Console.WriteLine("2. Remove task!");
+				Console.WriteLine("3. Viev task!");
+				Console.WriteLine("4. Exit!");
+				int option = GetIntInRange(4);
+
+				if (option == 1)
+				{
+					task.AddTask();
+                    Console.WriteLine("Task added!");
+                }
+				else if (option == 2)
+				{
+					task.RemoveTask();
+                    Console.WriteLine("Task removed!");
+                }
+				else if (option == 3)
+				{
+					task.VievTask();
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+
+		static int GetIntInRange(int optrionalNumber)
+		{
+			string choose = Console.ReadLine();
+			int number = -1;
+			bool isConverted = int.TryParse(choose, out number);
+			bool isInRange = number >= 1 && number <= optrionalNumber;
+
+			while (!isConverted || !isInRange)
+			{
+				Console.WriteLine("Error!");
+				choose = Console.ReadLine();
+				isConverted = int.TryParse(choose, out number);
+				isInRange = number >= 1 && number <= optrionalNumber;
+			}
+			return number;
+		}
+	}
+}
---
+++ Task List/Task.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_List
+{
+	public class Task
+	{
+		private List<string> addTask = new List<string>();
+		private List<string> descriptionTask = new List<string>();
+		public void AddTask()
+		{
+			Console.WriteLine("Name Task: ");
+			addTask.Add(Console.ReadLine());
+			Console.WriteLine("Description: ");
+			descriptionTask.Add(Console.ReadLine());
+		}
+
+		public void RemoveTask()
+		{
+			Console.WriteLine("Enter the name of the task to remove: ");
+			string taskNameToRemove = Console.ReadLine();
+
+			// Поиск индекса задачи по имени
+			int indexToRemove = addTask.IndexOf(taskNameToRemove);
+
+			if (indexToRemove >= 0)
+			{
+				// Если задача найдена, удаляем ее из обоих списков
+				addTask.RemoveAt(indexToRemove);
+				descriptionTask.RemoveAt(indexToRemove);
+
+				Console.WriteLine($"Task '{taskNameToRemove}' removed successfully.");
+			}
+			else
+			{
+				Console.WriteLine($"Task '{taskNameToRemove}' not found.");
+			}
+		}
+
+		public void VievTask()
+		{
+			for (int i = 0; i < addTask.Count; i++)
+			{
+				Console.WriteLine($"Task {i + 1}:");
+				Console.WriteLine($"Name: {addTask[i]}");
+				Console.WriteLine($"Description: {descriptionTask[i]}");
+			}
+		}
+	}
+}
